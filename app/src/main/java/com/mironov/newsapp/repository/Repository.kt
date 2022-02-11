4a6f96223e37d879f4ad9fe215f32ac826b2ea00
vivotab8 <mironoff2007@rambler.ru>
2022-02-11 23:50:18 +0300

    missed file

---
+++ app/src/main/java/com/mironov/newsapp/repository/Repository.kt
@@ -0,0 +1,22 @@
+import android.content.Context
+import android.content.SharedPreferences
+
+class Repository(context: Context){
+
+    companion object {
+        private const val KEY_IS_FIRST_STARTUP = "KEY_IS_FIRST_STARTUP"
+        private const val SHARED_PREFERENCES_NAME = "SHARED_PREFERENCES_NAME"
+    }
+
+    private val pref: SharedPreferences = context.getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE)
+    private val editor: SharedPreferences.Editor = pref.edit()
+
+    fun setNotFirstStartUp(){
+        editor.putBoolean(KEY_IS_FIRST_STARTUP,true).apply()
+    }
+
+    fun isFirstStartUp(): Boolean {
+       return pref.getBoolean(KEY_IS_FIRST_STARTUP,false)
+    }
+
+}
