7afd3c3c30a96b294a3a9cbd6ab6d710f84c5d79
Mironov Ury <mironoff2007@rambler.ru>
2022-02-12 10:35:30 +0300

    implement repository methods to check first startup

--- app/src/main/java/com/mironov/newsapp/repository/Repository.kt
+++ app/src/main/java/com/mironov/newsapp/repository/Repository.kt
@@ -1,8 +1,16 @@
 package com.mironov.newsapp.repository
-import com.mironov.newsapp.repository.DataShared
+
 import com.mironov.newsapp.repository.retrofit.NewsApi
 import javax.inject.Inject
 
 class Repository @Inject constructor (protected var dataShared: DataShared, protected var retrofit: NewsApi){
+
+    fun setNotFirstStartUp(){
+        dataShared.setNotFirstStartUp()
+    }
+
+    fun isFirstStartUp(): Boolean {
+        return dataShared.isFirstStartUp()
+    }
 
 }
