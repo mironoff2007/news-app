9c3c39510ed652fcfb1aead747734019227fff32
Mironov Ury <mironoff2007@rambler.ru>
2022-02-12 19:43:18 +0300

    Request articles as Single

--- app/src/main/java/com/mironov/newsapp/repository/retrofit/JsonResponse.kt
+++ app/src/main/java/com/mironov/newsapp/repository/retrofit/JsonResponse.kt
@@ -1,10 +1,15 @@
 package com.mironov.newsapp.repository.retrofit
+import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 import java.util.ArrayList
 
 class JsonResponse {
     @SerializedName("status")
     var status: String? = null
+
+    @Expose
+    @SerializedName("message")
+    var message: String? = null
 
     @SerializedName("articles")
     var articles: ArrayList<Article>?=null
