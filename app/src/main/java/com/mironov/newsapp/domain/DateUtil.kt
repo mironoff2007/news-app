168dbaf7a8b2b5c3c8b27a84ef9f0f303377090f
Mironov Ury <mironoff2007@rambler.ru>
2022-05-05 18:34:38 +0300

    correctly catch exceptions

--- app/src/main/java/com/mironov/newsapp/domain/DateUtil.kt
+++ app/src/main/java/com/mironov/newsapp/domain/DateUtil.kt
@@ -8,13 +8,14 @@ import java.util.*
 object DateUtil {
 
     private const val PATTERN_DATE_FORMAT = "yyyy-MM-dd"
-    private const val API_DATE_FORMAT = "yyyy-MM-dd"
+    private const val API_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm"
     private const val UI_DATE_FORMAT = "dd MMM yyyy"
+    private const val UI_TIME_FORMAT = "HH:mm dd MMM"
 
     fun getTodayDate(): String {
         val cal: Calendar = Calendar.getInstance()
         val year: Int = cal.get(Calendar.YEAR)
-        val month: Int = cal.get(Calendar.MONTH)+1
+        val month: Int = cal.get(Calendar.MONTH) + 1
         val day: Int = cal.get(Calendar.DAY_OF_MONTH)
 
         return  formatDate(day, month, year)
@@ -24,7 +25,7 @@ object DateUtil {
         val cal: Calendar = Calendar.getInstance()
         cal.add(Calendar.DATE, -daysBack)
         val year: Int = cal.get(Calendar.YEAR)
-        val month: Int = cal.get(Calendar.MONTH)+1
+        val month: Int = cal.get(Calendar.MONTH) + 1
         val day: Int = cal.get(Calendar.DAY_OF_MONTH)
 
         return  formatDate(day, month, year)
@@ -32,21 +33,38 @@ object DateUtil {
 
     @SuppressLint("SimpleDateFormat")
     fun formatDate(day: Int, month: Int, year: Int): String {
-        val date = SimpleDateFormat(PATTERN_DATE_FORMAT).parse("$year-$month-$day")
-        return SimpleDateFormat(API_DATE_FORMAT).format(date)
+        return try {
+            val date = SimpleDateFormat(PATTERN_DATE_FORMAT).parse("$year-$month-$day")
+            SimpleDateFormat(PATTERN_DATE_FORMAT).format(date)
+        } catch (e: Exception) {
+            ""
+        }
     }
 
     @SuppressLint("SimpleDateFormat")
     fun getDate(dateString: String?): String {
         return try {
-            SimpleDateFormat(API_DATE_FORMAT).parse(dateString).toString()
-        } catch (e: java.text.ParseException) {
+            SimpleDateFormat(PATTERN_DATE_FORMAT).parse(dateString ?: "")?.toString() ?: ""
+        } catch (e: Exception) {
             ""
         }
     }
 
     fun convertDate(date: String): String {
-        val date = SimpleDateFormat(API_DATE_FORMAT).parse(date)
-        return SimpleDateFormat(UI_DATE_FORMAT, Locale.getDefault()).format(date)
+        return try {
+            val date = SimpleDateFormat(API_DATE_FORMAT).parse(date)
+            SimpleDateFormat(UI_DATE_FORMAT, Locale.getDefault()).format(date)
+        } catch (e: Exception) {
+            ""
+        }
+    }
+
+    fun convertTime(date: String): String {
+        return try {
+            val date = SimpleDateFormat(API_DATE_FORMAT).parse(date)
+            SimpleDateFormat(UI_TIME_FORMAT, Locale.getDefault()).format(date)
+        } catch (e: Exception) {
+            ""
+        }
     }
 }
